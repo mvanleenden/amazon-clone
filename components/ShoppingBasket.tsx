f5a025f56d00b1abeae2bc49a9d0b4e749b5787e
Mayron van Leenden <mvanleenden@gmail.com>
2021-09-09 20:41:02 +0200

    shopping basket view WIP

---
+++ components/ShoppingBasket.tsx
@@ -0,0 +1,9 @@
+const ShoppingBasket = () => {
+  return (
+    <div className="flex flex-col p-5 space-y-10 bg-white">
+      <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+    </div>
+  );
+};
+
+export default ShoppingBasket;
