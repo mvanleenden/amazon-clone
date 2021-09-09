31a09adfa67ef23c795a5e42f0ac842df3578c02
Mayron van Leenden <mvanleenden@gmail.com>
2021-09-09 20:42:03 +0200

    added link to logo and shopping basket

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { MenuIcon, ShoppingCartIcon } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/client";
 
@@ -6,12 +7,14 @@ import SearchBar from "./SearchBar";
 
 const Header = () => {
   const [session, loading] = useSession();
+  const router = useRouter();
 
   return (
     <header>
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="/amazon_logo.png"
             width={150}
             height={40}
@@ -39,7 +42,10 @@ const Header = () => {
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="relative header__link flex items-center">
+          <div
+            onClick={() => router.push("/checkout")}
+            className="relative header__link flex items-center"
+          >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 rounded-full text-center text-black font-bold">
               0
             </span>
